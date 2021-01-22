e16dcab5e7f8017568079a5d752b3b932148426a
Zahnen Garner <zahnen@gmail.com>
2021-01-22 08:32:20 -0800

    add Shop model logic

---
+++ Models/Shop.cs
@@ -0,0 +1,10 @@
+namespace ShopDinePortland.Models
+{
+  public class Shop
+  {
+    public int ShopId { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Neighborhood { get; set; }
+  }
+}
