d9b81f4589ad33e6d5f6e02f36d8fedd54147ed7
Zahnen Garner <zahnen@gmail.com>
2021-01-22 08:29:01 -0800

    add Restaurant model logic

---
+++ Models/Restaurant.cs
@@ -0,0 +1,11 @@
+namespace ShopDinePortland.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Cuisine { get; set; }
+    public string Service { get; set; }
+    public string Neighborhood { get; set; }
+  }
+}
