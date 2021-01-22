f77fbf0b592d1202be1fc251ca9160542f616894
Zahnen Garner <zahnen@gmail.com>
2021-01-22 08:54:07 -0800

    add RestaurantsController, GET method for Restaurants

---
+++ Controllers/RestaurantsController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopDinePortland.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class RestaurantsController : ControllerBase
+  {
+    private ShopDinePortlandContext _db;
+
+    public RestaurantsController(ShopDinePortlandContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Restaurant>> Get()
+    {
+      return _db.Restaurants.ToList();
+    }
+  }
+}
