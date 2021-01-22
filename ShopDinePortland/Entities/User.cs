3177d2d361be9113638059d2ffc79ab3714fcb9d
Zahnen Garner <zahnen@gmail.com>
2021-01-22 11:55:17 -0800

    add Role and User Entities for authentication

---
+++ ShopDinePortland/Entities/User.cs
@@ -0,0 +1,13 @@
+namespace ShopDinePortland.Entities
+{
+  public static class Role
+  {
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+    public string Token { get; set; }
+  }
+}
